07f2ac1c33a7b68bb99faf35b23c9c7d9e99b520
Begari Rajshekar <b.rajshekar1212@gmail.com>
2025-11-26 11:00:10 +0530

    Update image.dart

--- Week-6/image.dart
+++ Week-6/image.dart
@@ -1,29 +1,37 @@
-import 'package:flutter/material.dart';   
- void main() => runApp(MyApp());   
- class MyApp extends StatelessWidget {   
-  @override   
-  Widget build(BuildContext context) {   
-    return MaterialApp(   
-      home: Scaffold(   
-        appBar: AppBar(   
-            title: Text('Flutter Image Demo'),   
-        ),   
-        body: Center(   
-          child: Column(   
-            children: <Widget>[   
-              Image.asset('assets/tablet.png'),   
-              Text(   
-                  'A tablet is a wireless touch screen computer that is smaller than a notebook b
- ut larger than a smartphone.',   
-                  style: TextStyle(fontSize: 20.0),   
-              )   
-            ],   
-          ),   
-        ),   
-      ),   
-    );   
-  }   
-}   
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Image Demo'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/tablet.png'),
+              const SizedBox(height: 20),
+              const Text(
+                'A tablet is a wireless touch screen computer that is smaller '
+                'than a notebook but larger than a smartphone.',
+                style: TextStyle(fontSize: 20.0),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
 
 
